ac9f216b1476c39f0d95bb08c51496120304ca0e
robintun <robintun01@gmail.com>
2023-10-16 21:42:23 -0700

    changed code for asgn3

--- src/api/audit.py
+++ src/api/audit.py
@@ -15,13 +15,27 @@ router = APIRouter(
 @router.get("/inventory")
 def get_inventory():
     """ """
-    with db.engine.begin() as connection:
-        result = connection.execute(sqlalchemy.text(""" SELECT * FROM global_inventory"""))
-    first_row = result.first()
+    # Assignement 2
+    # with db.engine.begin() as connection:
+    #     global_inventory = connection.execute(sqlalchemy.text(""" SELECT * FROM global_inventory """))
+    # first_row = result.first()
 
-    total_potions = first_row.num_red_potions + first_row.num_green_potions + first_row.num_blue_potions
-    total_ml = first_row.num_red_ml + first_row.num_green_ml + first_row.num_blue_ml
-    my_gold = first_row.gold
+    # total_potions = first_row.num_red_potions + first_row.num_green_potions + first_row.num_blue_potions
+    # total_ml = first_row.num_red_ml + first_row.num_green_ml + first_row.num_blue_ml
+    # my_gold = first_row.gold
+
+    # return {"number_of_potions": total_potions, "ml_in_barrels": total_ml, "gold": my_gold}
+
+    with db.engine.begin() as connection:
+        global_inventory = connection.execute(sqlalchemy.text(""" SELECT * FROM global_inventory """))
+        my_catalog = connection.execute(sqlalchemy.text(""" SELECT inventory FROM potions """))
+
+    total_potions = 0
+    for each_potion in my_catalog:
+        total_potions += each_potion.inventory
+        
+    total_ml = global_inventory.num_red_ml + global_inventory.num_green_ml + global_inventory.num_blue_ml
+    my_gold = global_inventory.gold
 
     return {"number_of_potions": total_potions, "ml_in_barrels": total_ml, "gold": my_gold}
 
