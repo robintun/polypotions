be06c069759b6c6419a7f97c27edd193d3b97882
robintun <robintun01@gmail.com>
2023-10-16 21:30:07 -0700

    implemented catalog for asgn3

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -11,42 +11,62 @@ def get_catalog():
     """
     Each unique item combination must have only a single price.
     """
-    with db.engine.begin() as connection:
-        result = connection.execute(sqlalchemy.text(""" SELECT num_red_potions,
-        num_green_potions, num_blue_potions FROM global_inventory """))
-    first_row = result.first()
+    # Assignment 2
+    # with db.engine.begin() as connection:
+    #     result = connection.execute(sqlalchemy.text(""" SELECT num_red_potions,
+    #     num_green_potions, num_blue_potions FROM global_inventory """))
+    # first_row = result.first()
 
-    # Can return a max of 20 items.
+    # # Can return a max of 20 items.
+
+    # my_catalog = []
+    # if (first_row.num_red_potions > 0):
+    #     my_catalog.append(
+    #         {
+    #             "sku": "RED_POTION_0",
+    #             "name": "red potion",
+    #             "quantity": first_row.num_red_potions,
+    #             "price": 50,
+    #             "potion_type": [100, 0, 0, 0],
+    #         }
+    #     )
+    # if (first_row.num_green_potions > 0):
+    #     my_catalog.append(
+    #         {
+    #             "sku": "GREEN_POTION_0",
+    #             "name": "green potion",
+    #             "quantity": first_row.num_green_potions,
+    #             "price": 50,
+    #             "potion_type": [0, 100, 0, 0],
+    #         }
+    #     )
+    # if (first_row.num_blue_potions > 0):
+    #     my_catalog.append(
+    #         {
+    #             "sku": "BLUE_POTION_0",
+    #             "name": "blue potion",
+    #             "quantity": first_row.num_blue_potions,
+    #             "price": 50,
+    #             "potion_type": [0, 0, 100, 0],
+    #         }
+    #     )
+    # return my_catalog
 
     my_catalog = []
-    if (first_row.num_red_potions > 0):
-        my_catalog.append(
-            {
-                "sku": "RED_POTION_0",
-                "name": "red potion",
-                "quantity": first_row.num_red_potions,
-                "price": 50,
-                "potion_type": [100, 0, 0, 0],
-            }
-        )
-    if (first_row.num_green_potions > 0):
-        my_catalog.append(
-            {
-                "sku": "GREEN_POTION_0",
-                "name": "green potion",
-                "quantity": first_row.num_green_potions,
-                "price": 50,
-                "potion_type": [0, 100, 0, 0],
-            }
-        )
-    if (first_row.num_blue_potions > 0):
-        my_catalog.append(
-            {
-                "sku": "BLUE_POTION_0",
-                "name": "blue potion",
-                "quantity": first_row.num_blue_potions,
-                "price": 50,
-                "potion_type": [0, 0, 100, 0],
-            }
-        )
+
+    with db.engine.begin() as connection:
+        result = connection.execute(sqlalchemy.text(""" SELECT sku,
+        price,inventory,potion_type FROM potions """))
+    
+    for each_potion in result:
+        if (each_potion.inventory > 0):
+            my_catalog.append(
+                {
+                    "sku": each_potion.sku,
+                    "name": each_potion.sku,
+                    "quantity": each_potion.inventory,
+                    "price": each_potion.price,
+                    "potion_type": each_potion.potion_type,
+                }
+            )
     return my_catalog
