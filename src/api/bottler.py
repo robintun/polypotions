af415f6b5199725f18eb236003fa5db915c0127e
robintun <robintun01@gmail.com>
2023-10-23 23:38:34 -0700

    ledgerized bottler

--- src/api/bottler.py
+++ src/api/bottler.py
@@ -89,13 +89,21 @@ def post_deliver_bottles(potions_delivered: list[PotionInventory]):
                                             [{"additional_potions": potion_delivered.quantity,
                                             "potion_type": potion_delivered.potion_type}])
             
-            connection.execute(sqlalchemy.text(""" UPDATE global_inventory SET
-                                                   num_red_ml = num_red_ml - :red_ml,
-                                                   num_green_ml = num_green_ml - :green_ml,
-                                                   num_blue_ml = num_blue_ml - :blue_ml,
-                                                   num_dark_ml = num_dark_ml - :dark_ml """),
-                            [{"red_ml": red_ml, "green_ml": green_ml, "blue_ml": blue_ml, "dark_ml": dark_ml}])
-
+            # connection.execute(sqlalchemy.text(""" UPDATE global_inventory SET
+            #                                        num_red_ml = num_red_ml - :red_ml,
+            #                                        num_green_ml = num_green_ml - :green_ml,
+            #                                        num_blue_ml = num_blue_ml - :blue_ml,
+            #                                        num_dark_ml = num_dark_ml - :dark_ml """),
+            #                 [{"red_ml": red_ml, "green_ml": green_ml, "blue_ml": blue_ml, "dark_ml": dark_ml}])
+            connection.execute(sqlalchemy.text(""" INSERT INTO potions_ledger (change_of_potion, potion_id)
+                                                   SELECT :change_of_potion, potions.id
+                                                   FROM potions
+                                                   WHERE potions.potion_type = :potion_type """),
+                                            [{"change_of_potion": potion_delivered.quantity, "potion_type": potion_delivered.potion_type}])
+            
+            connection.execute(sqlalchemy.text(""" INSERT INTO ml_ledger (red_ml_change, green_ml_change, blue_ml_change, dark_ml_change)
+                                                   VALUES (:red_ml, :green_ml, :blue_ml, :dark_ml) """),
+                                            [{"red_ml": -red_ml, "green_ml": -green_ml, "blue_ml": -blue_ml, "dark_ml": -dark_ml}])
     return "OK"
 
 # Gets called 4 times a day
