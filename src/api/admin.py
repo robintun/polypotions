56612cd285cc318a98a07ef716ee02f05751bebc
robintun <robintun01@gmail.com>
2023-10-30 17:37:14 -0700

    updated the reset and shop_info func

--- src/api/admin.py
+++ src/api/admin.py
@@ -14,6 +14,22 @@ def reset():
     Reset the game state. Gold goes to 100, all potions are removed from
     inventory, and all barrels are removed from inventory. Carts are all reset.
     """
+
+    with db.engine.begin() as connection:
+        connection.execute(sqlalchemy.text(""" TRUNCATE cart, cart_items, 
+                                               gold_ledger, ml_ledger, potions_ledger """))
+        
+        connection.execute(sqlalchemy.text(""" INSERT INTO gold_ledger (change_of_gold)
+                                               VALUES (100) """))
+
+        connection.execute(sqlalchemy.text(""" INSERT INTO ml_ledger (red_ml_change, green_ml_change, 
+                                               blue_ml_change, dark_ml_change)
+                                               VALUES (0,0,0,0) """))
+        connection.execute(sqlalchemy.text(""" INSERT INTO potions_ledger (potion_id, change_of_potion)
+                                               SELECT potions.id, 0 
+                                               FROM potions
+                                               GROUP BY potions.id """))
+
     return "OK"
 
 
@@ -23,7 +39,7 @@ def get_shop_info():
 
     # TODO: Change me!
     return {
-        "shop_name": "Potion Shop",
-        "shop_owner": "Potion Seller",
+        "shop_name": "Poly Potions",
+        "shop_owner": "Robin",
     }
 
